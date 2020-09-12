4870fa93c787b4dc1ffacae78fbe96c1edc714f0
kolinapohta2000@gmail.com <k4994616595>
2020-09-12 17:26:39 +0300

    aDDED sOME sITE FUNCTIONALITY :)

--- Game/urls.py
+++ Game/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'user_page'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('test2/', views.to_MainWindow, name='to_MainWindow'),
+    path('test/', views.to_top_players, name='to_top_players'),
+    path('test3/', views.to_personal_page, name='to_personal_page'),
 ]
--- Game/views.py
+++ Game/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,3 +8,26 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "player/statistica.html")
+
+def to_MainWindow(request):
+    try:
+        pass
+    except:
+        raise Http404('Что-то пошло не так')
+    return render(request, "player/mainWindow.html")
+
+
+def to_top_players(request):
+    try:
+        pass
+    except:
+        raise Http404('Что-то пошло не так')
+    return render(request, "player/statistica.html")
+
+
+def to_personal_page(request):
+    try:
+        pass
+    except:
+        raise Http404('Что-то пошло не так')
+    return render(request, "player/Personal Page.html")
